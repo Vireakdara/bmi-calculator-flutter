817d68b5f4958c052ff7319bc8e16005e7945d01
ly vireakdara <ly.vireakdara2017@rupp.edu.kh>
2021-01-28 23:07:45 +0700

    up-to-date

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,8 @@
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'icon_content.dart';
 
 const bottomContainerHight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
@@ -22,9 +26,17 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 ReusableCard(
+                  cardChild: IconContent(
+                    label: "MALE",
+                    icon: FontAwesomeIcons.mars,
+                  ),
                   colour: activeCardColour,
                 ),
                 ReusableCard(
+                  cardChild: IconContent(
+                    label: "FEMALE",
+                    icon: FontAwesomeIcons.venus,
+                  ),
                   colour: activeCardColour,
                 ),
               ],
@@ -58,24 +70,6 @@ class _InputPageState extends State<InputPage> {
             height: bottomContainerHight,
           ),
         ],
-      ),
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  final Color colour;
-  ReusableCard({@required this.colour});
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: colour,
-          borderRadius: BorderRadius.circular(10.0),
-        ),
       ),
     );
   }
