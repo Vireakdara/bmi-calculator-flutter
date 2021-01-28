b02136e1043b9cf5719028579041f567c04f3d64
ly vireakdara <ly.vireakdara2017@rupp.edu.kh>
2021-01-28 23:12:58 +0700

    up-to-date

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+const labelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
 class IconContent extends StatelessWidget {
   IconContent({this.icon, this.label});
 
@@ -15,10 +20,7 @@ class IconContent extends StatelessWidget {
         SizedBox(height: 15.0),
         Text(
           label,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF8D8E98),
-          ),
+          style: labelTextStyle,
         )
       ],
     );
